f64a5964b08e60d7ecc7872f651e60d6a688022f
Brandon Batie <brandonbatie@gmail.com>
2022-09-20 15:12:21 -0600

    fixed destroy profile and account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update]
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+   before_action :require_user, only: [:edit, :update]
+   before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
 def index 
   @users = User.paginate(page: params[:page], per_page: 5)
@@ -26,8 +29,6 @@ end
       else
         render 'edit', status: :unprocessable_entity   
       end
-
-   
   end
 
   def create 
@@ -39,16 +40,30 @@ end
     else 
       render 'new', status: :unprocessable_entity    
     end 
-
   end
 
+def destroy
+     @user.destroy
+     session[:user_id] = nil if @user == current_user
+     flash[:notice] = "Account and all associated articles successfully deleted"
+     redirect_to root_path, status: :see_other
+   end
 
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password )
   end  
 
-  def set_user 
+  def set_user  
      @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
+    end
+  end
+
+
 end
